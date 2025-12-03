1500da0a871acd6c90294ce67d23107175d005ff
LarsGraenicher <lars2002@bluewin.ch>
2025-12-03 16:46:53 +0100

    Stand nach der ersten Implementierung des Backends

--- main.py
+++ main.py
@@ -1,8 +1,87 @@
 from fastapi import FastAPI
+import pandas as pd
+
+
 
 app = FastAPI()
 
+data= pd.read_csv(r"C:\Users\lars2\FHNW_lokal\WID\Projekt\Projekt_HS_25\src\Gesamtdatensatz.csv")
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
+
 
 @app.get("/v1/pedestrians_count")
-def erkundung(date: str = "21.04.2024"): #define a function that takes a name as a query parameter
-    return f"Datum {date}!"
+def erkundung(date: str = "2024-04-21"): #define a function that takes a name as a query parameter
+
+    data_filtered_date=pd.DataFrame()
+    for hour in range (24):
+        dataframe_hour= data[(data['timestamp'] == f"{date}T{hour:02d}:00:00Z")]
+    
+        location_name= "Bahnhofstrasse (Nord)"
+    
+        for i in range (2):
+            maske=(dataframe_hour["location_name"] == location_name)
+
+            ltr_label=dataframe_hour.loc[maske, "ltr_label"]  #.loc und .iloc werden gebraucht um wirklich auf den Wert zuzugreifen.
+                                                                    #Ohne diese Tools wird eine Serie "Ausschnitt aus dem Dataframe gemacht"
+                                                                    #Dieser hat den nicht den Datentyp str sonder object und denn kann man dann nicht weiterverwenden
+            rtl_label=dataframe_hour.loc[maske, "rtl_label"]
+            dataframe_hour.loc[maske, "ltr_label"]=rtl_label
+            dataframe_hour.loc[maske, "rtl_label"]=ltr_label
+
+            ltr_pedestrians_count = dataframe_hour.loc[maske, "ltr_pedestrians_count"]
+            rtl_pedestrians_count = dataframe_hour.loc[maske, "rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "ltr_pedestrians_count"] = rtl_pedestrians_count
+            dataframe_hour.loc[maske, "rtl_pedestrians_count"] = ltr_pedestrians_count
+
+            adult_ltr_pedestrians_count = dataframe_hour.loc[maske, "adult_ltr_pedestrians_count"]
+            adult_rtl_pedestrians_count = dataframe_hour.loc[maske, "adult_rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "adult_ltr_pedestrians_count"] = adult_rtl_pedestrians_count
+            dataframe_hour.loc[maske, "adult_rtl_pedestrians_count"] = adult_ltr_pedestrians_count
+
+            child_ltr_pedestrians_count = dataframe_hour.loc[maske, "child_ltr_pedestrians_count"]
+            child_rtl_pedestrians_count = dataframe_hour.loc[maske, "child_rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "child_ltr_pedestrians_count"] = child_rtl_pedestrians_count
+            dataframe_hour.loc[maske, "child_rtl_pedestrians_count"] = child_ltr_pedestrians_count
+
+            zone_1_ltr_pedestrians_count = dataframe_hour.loc[maske, "zone_1_ltr_pedestrians_count"]
+            zone_1_rtl_pedestrians_count = dataframe_hour.loc[maske, "zone_1_rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "zone_1_ltr_pedestrians_count"] = zone_1_rtl_pedestrians_count
+            dataframe_hour.loc[maske, "zone_1_rtl_pedestrians_count"] = zone_1_ltr_pedestrians_count
+
+            zone_2_ltr_pedestrians_count = dataframe_hour.loc[maske, "zone_2_ltr_pedestrians_count"]
+            zone_2_rtl_pedestrians_count = dataframe_hour.loc[maske, "zone_2_rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "zone_2_ltr_pedestrians_count"] = zone_2_rtl_pedestrians_count
+            dataframe_hour.loc[maske, "zone_2_rtl_pedestrians_count"] = zone_2_ltr_pedestrians_count
+
+            zone_3_ltr_pedestrians_count = dataframe_hour.loc[maske, "zone_3_ltr_pedestrians_count"]
+            zone_3_rtl_pedestrians_count = dataframe_hour.loc[maske, "zone_3_rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "zone_3_ltr_pedestrians_count"] = zone_3_rtl_pedestrians_count
+            dataframe_hour.loc[maske, "zone_3_rtl_pedestrians_count"] = zone_3_ltr_pedestrians_count
+
+            zone_99_ltr_pedestrians_count = dataframe_hour.loc[maske, "zone_99_ltr_pedestrians_count"]
+            zone_99_rtl_pedestrians_count = dataframe_hour.loc[maske, "zone_99_rtl_pedestrians_count"]
+            dataframe_hour.loc[maske, "zone_99_ltr_pedestrians_count"] = zone_99_rtl_pedestrians_count
+            dataframe_hour.loc[maske, "zone_99_rtl_pedestrians_count"] = zone_99_ltr_pedestrians_count
+
+
+            location_name = "Bahnhofstrasse (Süd)"
+
+
+    
+        data_filtered_date=pd.concat([data_filtered_date, dataframe_hour])
+    data_fragestellung=data_filtered_date.to_json( orient="records", indent=2) #orient="records", indent=2 orient ist dafür da um ein Array zu erhalten und nicht ein Dictionary, ident für bessere lesbarkeit nicht alles auf einer Zeile
+
+
+
+    return data_fragestellung
--- src/Fragestellung.jsx
+++ src/Fragestellung.jsx
@@ -3,15 +3,33 @@ import Button from "@mui/material/Button";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useEffect, useState } from "react";
 
-import fragestellung_json from "./jsons/fragestellung_json.json";
+import fragestellung_json_spec from "./jsons/fragestellung_spec.json";
 
 export const Fragestellung = ({ setSeite }) => {
+  const [data, setData] = useState([]);
+  const [spec, setSpec] = useState(fragestellung_json_spec);
+
+  useEffect(() => {
+    setSpec({
+      ...fragestellung_json_spec,
+      data: { values: data },
+    });
+    console.log(spec);
+  }, [data]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/v1/pedestrians_count?date=2024-12-10")
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, []);
+
   return (
     <div className="Fragestellung">
       <h2>Inhaltlicher Titel</h2>
 
-      <VegaEmbed spec={fragestellung_json} />
+      <VegaEmbed spec={spec} />
       <div>
         Wie sieht es an Ihrem Geburtstag aus?
         <LocalizationProvider dateAdapter={AdapterDayjs}>
